faa1075a226da02b358a59d1fdb0bb8571072701
Miguel Ferral <mferral@gmail.com>
2021-07-20 11:37:57 -0500

    Update 20 Julio

--- authorization/views.py
+++ authorization/views.py
@@ -12,7 +12,9 @@ class AuthToken(ObtainAuthToken):
         serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
-        # Buscar y eliminar token                                            
+        # Buscar y eliminar token 
+        # 
+        #                                            
         token, created = Token.objects.get_or_create(user=user)
         print (user)
         return Response({
